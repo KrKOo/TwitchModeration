c7590844a0fd7f2e9a377d9b7fe1f33c1cc0696a
KrKOo <krkoo.123@gmail.com>
2021-06-14 13:09:44 +0200

    ADD: .editorconfig

--- client/src/screens/Home/Home.js
+++ client/src/screens/Home/Home.js
@@ -1,4 +1,3 @@
-import logo from '../../components/images/logo.svg';
 import './Home.scss';
 import Header from '../../components/Header/Header'
 
@@ -6,20 +5,6 @@ function Home() {
   return (
     <div className="Home">
       <Header />
-      <header className="Home-header">
-        <img src={logo} className="Home-logo" alt="logo" />
-        <p>
-          Edit <code>src/Home.js</code> and save to reload.
-        </p>
-        <a
-          className="Home-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Random button
-        </a>
-      </header>
     </div>
   );
 }
