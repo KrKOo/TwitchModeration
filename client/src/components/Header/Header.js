1a38a2b877d95124028e711ffede64d0a6f2a161
KrKOo <krkoo.123@gmail.com>
2021-06-14 13:09:44 +0200

    ADD: .editorconfig

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import logo from '../../../components/images/logo.svg';
+import logo from '../images/logo.svg';
 import './Header.scss';
 
 class Header extends React.Component {
+  handleClick = (e) => {
+    console.log(e);
+  }
+
   render() {
-    <header className="Header">
-      <img src={logo} className="Header-logo" alt="logo" />
+    return (
+      <header className="Header">
+        <img src={logo} className="Header-logo" alt="logo" />
 
-      <nav>
-        <ul>
-          <li onClick={this.handleClick(e)}></li>
-        </ul>
-      </nav>
-
-      <a
-        className="Home-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Random button
-        </a>
-    </header>
+        <nav>
+          <ul>
+            <li onClick={this.handleClick}>Home</li>
+            <li onClick={this.handleClick}>About</li>
+            <li onClick={this.handleClick}>How it works?</li>
+            <li onClick={this.handleClick}>Log In</li>
+          </ul>
+        </nav>
+      </header>
+    );
   }
 }
 
-export default Home;
+export default Header;
