0fd8a8c46043e5637231d857a205cb94d3329e6b
KrKOo <krkoo.123@gmail.com>
2021-06-18 13:30:32 +0200

    Dashboard setup

---
+++ client/src/screens/Dashboard/Components/ItemSelection/ItemSelection.js
@@ -0,0 +1,11 @@
+import styles from './ItemSelection.module.scss';
+
+function ItemSelection() {
+  return (
+    <div className={styles.ItemSelection}>
+
+    </div>
+  );
+}
+
+export default ItemSelection;
