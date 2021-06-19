f85d6e9d9779c4b78aeff3a15c41f62faba78bc5
KrKOo <krkoo.123@gmail.com>
2021-06-19 16:51:25 +0200

    Backend initial setup

---
+++ server/app.js
@@ -0,0 +1,44 @@
+import createError from 'http-errors';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import http from 'http';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import * as socketIO from './modules/sockets';
+
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+
+const app = express();
+
+const server = http.createServer(app);
+
+socketIO.listen(server);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+export default app;
