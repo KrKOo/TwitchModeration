358beeff1c2101a0f0d324d14d076872892230ec
KrKOo <krkoo.123@gmail.com>
2021-06-21 03:00:42 +0200

    Client socket connection

--- client/src/screens/Dashboard/Components/StreamOverlay/StreamOverlay.js
+++ client/src/screens/Dashboard/Components/StreamOverlay/StreamOverlay.js
@@ -1,10 +1,20 @@
+import React from 'react'
 import styles from './StreamOverlay.module.scss';
+import socketIOClient from 'socket.io-client';
 
 function StreamOverlay() {
+  const [response, setResponse] = React.useState("Ahoj");
+
+  React.useEffect(() => {
+    const socket = socketIOClient("/");
+    socket.on("message", data => {
+      setResponse(data.message);
+    });
+  });
 
   return (
     <div className={styles.StreamOverlay}>
-
+      {response}
     </div >
   );
 }
